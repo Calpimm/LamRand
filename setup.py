3144a387b4c7238c93b39bcb1da3ff900e8bf4af
calpimm <okirmak@gmail.com>
2024-07-02 00:15:58 +0300

    chore: Update .gitignore and setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='LamRand',
-    version='0.1',
+    version='0.1.0',
+    description='A high-quality random number generator library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/LamRand',
     packages=find_packages(),
     install_requires=[
         'openpyxl',
+        'scipy'
     ],
-    test_suite='tests',
-    entry_points={
-        'console_scripts': [
-            'lamrand=lamrand.lamrand:main',
-        ],
-    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
